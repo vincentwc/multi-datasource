e4ec51a559abf8fe7dcfac4b55434473ea4be29d
vincentwc <1340476074@qq.com>
2021-12-01 23:21:44 +0800

    mybatis-plus配置双数据源成功,并配置连接池

--- src/main/java/com/vincent/multidatasource/MultiDatasourceApplication.java
+++ src/main/java/com/vincent/multidatasource/MultiDatasourceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//		(exclude = {DruidDataSourceAutoConfigure.class})
 @MapperScan(basePackages = "com.vincent.multidatasource.dao")
 public class MultiDatasourceApplication {
 
